8903fb090a14d78faa764f95b646340e4724f39f
Charlie Niekirk <18074024+charlie-niekirk@users.noreply.github.com>
2026-09-05 18:22:53 +0100

    Enable Expressive theme, motion, type, and shapes

---
+++ core/designsystem/src/main/kotlin/com/fitdroid/core/designsystem/theme/Shape.kt
@@ -0,0 +1,15 @@
+package com.fitdroid.core.designsystem.theme
+
+import androidx.compose.material3.ShapeDefaults
+import androidx.compose.material3.Shapes
+
+val FitdroidShapes =
+    Shapes(
+        extraSmall = ShapeDefaults.ExtraSmall,
+        small = ShapeDefaults.Small,
+        medium = ShapeDefaults.Medium,
+        large = ShapeDefaults.Large,
+        largeIncreased = ShapeDefaults.LargeIncreased,
+        extraLarge = ShapeDefaults.ExtraLarge,
+        extraLargeIncreased = ShapeDefaults.ExtraLargeIncreased,
+    )
--- core/designsystem/src/main/kotlin/com/fitdroid/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/kotlin/com/fitdroid/core/designsystem/theme/Theme.kt
@@ -2,7 +2,9 @@ package com.fitdroid.core.designsystem.theme
 
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.MaterialExpressiveTheme
+import androidx.compose.material3.MotionScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -27,6 +29,7 @@ private val LightColorScheme =
         tertiary = RestorativeBlue40,
     )
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun FitdroidTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
@@ -51,9 +54,11 @@ fun FitdroidTheme(
             else -> LightColorScheme
         }
 
-    MaterialTheme(
+    MaterialExpressiveTheme(
         colorScheme = colorScheme,
+        motionScheme = MotionScheme.expressive(),
         typography = FitdroidTypography,
+        shapes = FitdroidShapes,
         content = content,
     )
 }
